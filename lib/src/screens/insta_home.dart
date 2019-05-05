7c3621559a8ab94a7e7eaa813512e7599805aad3
Zeeshan Syed <zsyed89@gmail.com>
2019-05-05 17:08:57 -0400

    Added more stuff to the insta_list

--- lib/src/screens/insta_home.dart
+++ lib/src/screens/insta_home.dart
@@ -3,7 +3,7 @@ import 'insta_body.dart';
 
 class InstaHome extends StatelessWidget {
   final topBar = new AppBar(
-    backgroundColor: new Color(0xff8faf8),
+    backgroundColor: new Color(0xfff8faf8),
     centerTitle: true,
     elevation: 1.0,
     leading: new Icon(Icons.camera_alt),
--- lib/src/screens/insta_list.dart
+++ lib/src/screens/insta_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'insta_stories.dart';
 
 class InstaList extends StatelessWidget {
@@ -11,13 +12,14 @@ class InstaList extends StatelessWidget {
       itemBuilder: (context, index) => index == 0
           ? new SizedBox(
               child: new InstaStories(),
-              height: deviceSize.height * 0.15, // Need to provide a height
+              height: deviceSize.height * 0.17, // Need to provide a height
             )
           : Column(
               mainAxisAlignment: MainAxisAlignment.start,
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
+                // 1st Row - Profile image, name and vertical more icon
                 Padding(
                   padding: const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
                   child: Row(
@@ -31,9 +33,10 @@ class InstaList extends StatelessWidget {
                             decoration: new BoxDecoration(
                               shape: BoxShape.circle,
                               image: new DecorationImage(
-                                  fit: BoxFit.fill,
-                                  image: new NetworkImage(
-                                      "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg")),
+                                fit: BoxFit.fill,
+                                image: new NetworkImage(
+                                    'https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg'),
+                              ),
                             ),
                           ),
                           new SizedBox(
@@ -52,6 +55,98 @@ class InstaList extends StatelessWidget {
                     ],
                   ),
                 ),
+
+                // 2nd Row - Big Image
+                Flexible(
+                  fit: FlexFit.loose,
+                  child: new Image.network(
+                    'https://images.pexels.com/photos/672657/pexels-photo-672657.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+
+                // 3rd Row - Like, comment, icons
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          new Icon(
+                            FontAwesomeIcons.heart,
+                          ),
+                          new SizedBox(
+                            width: 16.0,
+                          ),
+                          new Icon(
+                            FontAwesomeIcons.comment,
+                          ),
+                          new SizedBox(
+                            width: 16.0,
+                          ),
+                          new Icon(FontAwesomeIcons.paperPlane),
+                        ],
+                      ),
+                      new Icon(FontAwesomeIcons.bookmark)
+                    ],
+                  ),
+                ),
+
+                // 4th Row - Liked by
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    "Liked by pawankumar, pk and 528,331 others",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+
+                // 5th Row - Add comment text
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      new Container(
+                        height: 40.0,
+                        width: 40.0,
+                        decoration: new BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: new DecorationImage(
+                              fit: BoxFit.fill,
+                              image: new NetworkImage(
+                                  "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg")),
+                        ),
+                      ),
+                      new SizedBox(
+                        width: 10.0,
+                      ),
+                      Expanded(
+                        child: new TextField(
+                          decoration: new InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Add a comment...",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // 6th Row - Days ago
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    "1 Day Ago",
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                )
               ],
             ),
     );
